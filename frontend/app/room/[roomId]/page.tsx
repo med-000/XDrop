0254cb858b37a79baf3966614809a0ed4518b0ba
med-macbook <med.rk000@gmail.com>
2026-03-26 00:47:16 +0900

    refactor: prama の取得方法の変更

--- frontend/app/room/[roomId]/page.tsx
+++ frontend/app/room/[roomId]/page.tsx
@@ -1,15 +1,13 @@
-import { useEffect } from "react";
-import { Config } from "@/lib/config";
-import { startOffer } from "@/lib/peer";
 import RoomIdPage from "@/components/client/roomId-page";
 
 type PageProps = {
-  params: {
+  params: Promise<{
     roomId: string;
-  };
+  }>;
 };
-const Page = ({ params }: PageProps) => {
-  return <RoomIdPage roomId={params.roomId}></RoomIdPage>;
+const Page = async ({ params }: PageProps) => {
+  const { roomId } = await params;
+  return <RoomIdPage roomId={roomId}></RoomIdPage>;
 };
 
 export default Page;
